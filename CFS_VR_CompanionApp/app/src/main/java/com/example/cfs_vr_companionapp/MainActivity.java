b34d52ed11360e3ebae1370437b0f37e008fbe25
Sour-Warrior <kyle.miller4300@gmail.com>
2023-05-13 12:51:28 +0930

    Add a TCP server to app

--- CFS_VR_CompanionApp/app/src/main/java/com/example/cfs_vr_companionapp/MainActivity.java
+++ CFS_VR_CompanionApp/app/src/main/java/com/example/cfs_vr_companionapp/MainActivity.java
@@ -20,7 +20,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
-import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -45,34 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static DifficultySettings difficultySettings = new DifficultySettings();
 
-    BluetoothAdapter _bluetoothAdapter;
-    Set<BluetoothDevice> pairedDevices;
-    public ArrayList<BluetoothDevice> _devices;
-    public DeviceListAdapter deviceListAdapter;
-    ListView allPairedDevices;
-
-    private final BroadcastReceiver receiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Discovery has found a device. Get the BluetoothDevice
-                // object and its info from the Intent.
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                        ActivityCompat.requestPermissions(
-                                MainActivity.this,
-                                new String[]{Manifest.permission.BLUETOOTH_CONNECT},
-                                2
-                        );
-                        return;
-                    }
-                }
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-            }
-        }
-    };
+    private TCPServer server;
 
     private AppBarConfiguration appBarConfiguration;
 
@@ -89,40 +61,7 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-
-        _bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        Button BTButton = (Button) findViewById(R.id.BTBtn);
-        Button pairButton = (Button) findViewById(R.id.pairBtn);
-        allPairedDevices = (ListView) findViewById(R.id.allPairedDevices);
-
-        _devices  = new ArrayList<BluetoothDevice>();
-
-        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                ActivityCompat.requestPermissions(
-                        MainActivity.this,
-                        new String[]{Manifest.permission.BLUETOOTH_CONNECT},
-                        2
-                );
-                return;
-            }
-        }
-        pairedDevices = _bluetoothAdapter.getBondedDevices();
-        if (pairedDevices.size() > 0) {
-            for (BluetoothDevice d : pairedDevices) {
-                if (d.getName().contains("Quest")) {
-                    String deviceName = d.getName();
-                    String deviceAddress = d.getAddress();
-                    Log.i(TAG, "paired device: " + deviceName + " at " + deviceAddress);
-                    _devices.add(d);
-                    break;
-                }
-                Log.i(TAG, "Quest 2 not connected");
-            }
-        }
-
-        deviceListAdapter = new DeviceListAdapter(this, MainActivity.this, R.layout.device_adapter_view, _devices);
-        allPairedDevices.setAdapter(deviceListAdapter);
+        this.server = new TCPServer();
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -132,101 +71,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        BTButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "onClick: toggling BT");
-                toggleBT();
-            }
-        });
 
-        pairButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "onClick: pairing device");
-                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                        ActivityCompat.requestPermissions(
-                                MainActivity.this,
-                                new String[] {Manifest.permission.BLUETOOTH_CONNECT},
-                                2
-                        );
-
-                        return;
-                    }
-                }
-                _bluetoothAdapter.startDiscovery();
-            }
-        });
     }
 
-    public void toggleBT() {
-        if (_bluetoothAdapter == null) {
-            Log.d(TAG, "toggleBT: Does not have BT functionality");
-        }
-        if (!_bluetoothAdapter.isEnabled()) {
-            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                    ActivityCompat.requestPermissions(
-                            MainActivity.this,
-                            new String[] {Manifest.permission.BLUETOOTH_CONNECT},
-                            2
-                    );
-                    return;
-                }
-            }
-            startActivity(enableBTIntent);
-            IntentFilter BTIntentFilter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-            registerReceiver(receiver1, BTIntentFilter);
-        } if (_bluetoothAdapter.isEnabled()) {
-            _bluetoothAdapter.disable();
-            IntentFilter BTIntentFilter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-            registerReceiver(receiver1, BTIntentFilter);
-        }
-    }
 
-    private final BroadcastReceiver receiver1 = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (action.equals(_bluetoothAdapter.ACTION_STATE_CHANGED)) {
-                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, _bluetoothAdapter.ERROR);
-                switch (state) {
-                    case BluetoothAdapter.STATE_OFF:
-                        Log.d(TAG, "onReceive: STATE OFF");
-                        break;
-                    case BluetoothAdapter.STATE_TURNING_OFF:
-                        Log.d(TAG, "receiver: STATE OFF");
-                        break;
-                    case BluetoothAdapter.STATE_ON:
-                        Log.d(TAG, "receiver: STATE ON");
-                        break;
-                    case BluetoothAdapter.STATE_TURNING_ON:
-                        Log.d(TAG, "receiver: STATE TURNING ON");
-                        break;
-                }
-            }
-        }
-    };
-
-    private BroadcastReceiver receiver2 = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            final String action = intent.getAction();
-            if (action.equals(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED)) {
-                int mode = intent.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE, BluetoothAdapter.ERROR);
-                 switch (mode) {
-                     case BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE:
-                         Log.d(TAG, "receiver2: Discoverability Enabled");
-                         break;
-                     case BluetoothAdapter.SCAN_MODE_CONNECTABLE:
-                         Log.d(TAG, "receiver2: Discoverability enabled: able to connect");
-                         break;
-
-                 }
-            }
-        }
-
-    };
 
 
     @Override
